24e95a0c7c77bf2afbc9358b942e543a73a3ec90
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-11 16:05:49 +0100

    add AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        name:'AppHeader',
+    }
+</script>
+
+<template>
+    <header>
+        <img src="" alt="yu-gi-oh logo">
+        <span>Yu-Gi-Oh API</span>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    header{
+        padding: 1rem;
+        span{
+            font-size: 2.5rem;
+        }
+    }
+</style>
