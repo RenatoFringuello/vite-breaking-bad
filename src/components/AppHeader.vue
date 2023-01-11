280983cc7e5e8b3c6b9888be075d6d1007afabce
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-12 00:35:59 +0100

    add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,14 +6,19 @@
 
 <template>
     <header>
-        <img src="" alt="yu-gi-oh logo">
+        <img src="../assets/logo.webp" alt="yu-gi-oh logo">
         <span>Yu-Gi-Oh API</span>
     </header>
 </template>
 
 <style lang="scss" scoped>
     header{
-        padding: 1rem;
+        padding: 1rem 1rem 0;
+        img{
+            width: 100px;
+            padding-bottom: 1rem;
+            vertical-align: middle;
+        }
         span{
             font-size: 2.5rem;
         }
