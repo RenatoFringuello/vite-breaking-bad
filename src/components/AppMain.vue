e9402476e2f88e9a0b7825ae691f4c831d9a728d
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-12 00:29:24 +0100

    add filter archetype for shown and page change

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,18 +11,48 @@
         data() {
             return {
                 store,
+                nPage:0,
+                nPageToGet:25,
+                nCardFound : 0,
+                archetypes:['-- All --', '-- No Archetype --'],
+                archetypeSelected:'',
             }
         },
         methods: {
             getData(){
-                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=0')
+                axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?num=${this.nPageToGet}&offset=${this.nPage * this.nPageToGet}`)
                      .then((response)=>{
-                        store.cardsRaw = response.data.data;
+                        this.archetypes = ['-- All --', '-- No Archetype --'];
+                        this.store.cardsRaw = response.data.data;
+                        this.store.cardsRaw.forEach((card)=>{
+                            let archetype = card.archetype;
+                            if(archetype === undefined){
+                                archetype = this.archetypes[1];
+                            }
+                            if(!this.archetypes.includes(archetype)){
+                                this.archetypes.push(archetype);
+                            }
+                        });
                      })
                      .catch((error)=>{
                         console.error(`something went wrong :${error}`);
+                     }).then(()=>{
+                        this.archetypes.sort();
+                        this.nCardFound = this.store.cardsRaw.length;
                      })
 
+            },
+            setArchetype(archetype){
+                //set archetype to undefined if '-- none --' or empty string if '-- all --'
+                this.archetypeSelected = (archetype === this.archetypes[1]) ? undefined : (archetype === this.archetypes[0]) ? '' : archetype;
+                // filter by archetype to get the number 
+                const cards = this.store.cardsRaw.filter(card => (card.archetype === this.archetypeSelected));
+                //set the number to max if '-- all --' selected
+                this.nCardFound = (this.archetypeSelected === '') ? this.store.cardsRaw.length : cards.length;
+            },
+            setPage(direction){
+                this.nPage += (direction === 'prev') ? -1 : 1;;
+                this.getData();
             }
         },
         created(){
@@ -35,13 +65,23 @@
     <main>
         <div class="container">
             <div class="select-container">
-                <select name="" id=""></select>
+                <select>
+                    <option v-for="archetype in archetypes" :value="archetype"
+                            @click="setArchetype(archetype)"
+                            @keyup.Enter="setArchetype(archetype)">{{ archetype }}</option>
+                </select>
+                <div class="set-page-container">
+                    <button @click="setPage('prev')">Prev</button>
+                    <span>{{ nPage }}</span>
+                    <button @click="setPage('next')">Next</button>
+                </div>
             </div>
             <section>
-                <h4>Found {{ store.cardsRaw.length }} cards</h4>
+                <h4>Found {{ nCardFound }} cards</h4>
                 <div class="cards-container">
                     <AppMainCard 
                         v-for="card in store.cardsRaw"
+                        v-show="archetypeSelected === card.archetype || archetypeSelected === ''"
                         :cardName="card.name"
                         :cardArchetype="card.archetype"
                         :cardImgSrc="card.card_images[0].image_url"/>
@@ -60,10 +100,19 @@
         padding-bottom: 3rem;
 
         .select-container{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
             padding:1.5rem;
             select{
                 width: 150px;
                 padding: .5rem;
+            }
+            .set-page-container{
+                *{
+                    margin-left: 1rem;
+                }
+                
             }
         }
         section{
