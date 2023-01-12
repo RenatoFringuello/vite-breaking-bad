8260a99534ca75695e8814c231644c8bfd09dea6
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-12 16:26:39 +0100

    add AppMainCardsList

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,59 +1,57 @@
 <script>
-    import AppMainCard from './AppMainCard.vue'
     import axios from 'axios'
     import {store} from '../store.js'
+    import AppMainCardsList from './AppMainCardsList.vue'
 
     export default {
-        name:'AppMain',
-        components:{
-            AppMainCard,
+        name: "AppMain",
+        components: { 
+            AppMainCardsList, 
         },
         data() {
             return {
                 store,
-                apiUrl : 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
-                nPage:0,
-                nPageToGet:25,
+                apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+                nPage: 0,
+                nPageToGet: 25,
                 // nMaxPage : 0,
-                nCardFound : 0,
+                nCardFound: 0,
                 // archetypes:['-- All --', '-- No Archetype --'],
                 // archetypeSelected:'',
-            }
+            };
         },
         methods: {
-            getData(){
-                axios.get(this.apiUrl,{
-                        params:{
-                            sort:'name',
-                            num:this.nPageToGet,
-                            offset:this.nPageToGet * this.nPage
-                        }
-                    })
-                    .then((response)=>{
-                        // this.nMaxPage = Math.floor(response.data.meta.total_rows / this.nPageToGet);
-                        // // da rimuovere
-                        // this.archetypes = ['-- All --', '-- No Archetype --'];
-                        this.store.cardsRaw = response.data.data;
-
-                        // //replace this with a real getArchetypes
-                        // this.store.cardsRaw.forEach((card)=>{
-                        //     let archetype = card.archetype;
-                        //     if(archetype === undefined){
-                        //         archetype = this.archetypes[1];
-                        //     }
-                        //     if(!this.archetypes.includes(archetype)){
-                        //         this.archetypes.push(archetype);
-                        //     }
-                        // });
-                    })
-                    .catch((error)=>{
-                        console.error(`something went wrong :${error}`);
-                    })
-                    .then(()=>{
-                        // this.archetypes.sort();
-                        this.nCardFound = this.store.cardsRaw.length;
-                    })
-
+            getData() {
+                axios.get(this.apiUrl, {
+                    params: {
+                        sort: "name",
+                        num: this.nPageToGet,
+                        offset: this.nPageToGet * this.nPage
+                    }
+                })
+                    .then((response) => {
+                    // this.nMaxPage = Math.floor(response.data.meta.total_rows / this.nPageToGet);
+                    // // da rimuovere
+                    // this.archetypes = ['-- All --', '-- No Archetype --'];
+                    this.store.cardsRaw = response.data.data;
+                    // //replace this with a real getArchetypes
+                    // this.store.cardsRaw.forEach((card)=>{
+                    //     let archetype = card.archetype;
+                    //     if(archetype === undefined){
+                    //         archetype = this.archetypes[1];
+                    //     }
+                    //     if(!this.archetypes.includes(archetype)){
+                    //         this.archetypes.push(archetype);
+                    //     }
+                    // });
+                })
+                    .catch((error) => {
+                    console.error(`something went wrong :${error}`);
+                })
+                    .then(() => {
+                    // this.archetypes.sort();
+                    this.nCardFound = this.store.cardsRaw.length;
+                });
             },
             // setArchetype(archetype){
             //     //set archetype to undefined if '-- none --' or empty string if '-- all --'
@@ -68,9 +66,9 @@
             //     this.getData();
             // }
         },
-        created(){
+        created() {
             this.getData();
-        }
+        },
     }
 </script>
 
@@ -90,13 +88,8 @@
             </div>
             <section>
                 <h4>Found {{ nCardFound }} cards</h4>
-                <div class="cards-container">
-                    <AppMainCard 
-                        v-for="card in store.cardsRaw"
-                        :cardName="card.name"
-                        :cardArchetype="card.archetype"
-                        :cardImgSrc="card.card_images[0].image_url"/>
-                </div>
+                <!-- list -->
+                <AppMainCardsList />
             </section>
         </div>
     </main>
@@ -119,26 +112,16 @@
                 width: 150px;
                 padding: .5rem;
             }
-            .set-page-container{
-                *{
-                    margin-left: 1rem;
-                }
+            // .set-page-container{
+            //     *{
+            //         margin-left: 1rem;
+            //     }
                 
-            }
+            // }
         }
         section{
             background-color: white;
             padding: 3rem 3rem 2rem;
-            h4{
-                background-color: $card-found-bg;
-                color: white;
-                padding: 1.2rem;
-            }
-            .cards-container{
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-between;
-            }
         }
     }
 </style>
---
+++ src/components/AppMainCardsList.vue
@@ -0,0 +1,41 @@
+<script>
+    import {store} from '../store.js'
+    import AppMainCard from './AppMainCard.vue'
+
+    export default {
+        name:'AppMainCardsList',
+        components:{
+            AppMainCard,
+        },  
+        data() {
+            return {
+                store,
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="cards-container">
+        <AppMainCard 
+            v-for="card in store.cardsRaw"
+            :cardName="card.name"
+            :cardArchetype="card.archetype"
+            :cardImgSrc="card.card_images[0].image_url"/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+
+    h4{
+        background-color: $card-found-bg;
+        color: white;
+        padding: 1.2rem;
+    }
+    .cards-container{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
